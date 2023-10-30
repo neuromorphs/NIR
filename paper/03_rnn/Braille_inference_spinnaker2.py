563b1ec5cffa25a65e255f0d0fef54dba6ef67c5
Bernhard Vogginger <Bernhard.Vogginger@tu-dresden.de>
2023-10-30 10:22:56 +0100

    update S2 braille script

--- paper/03_rnn/Braille_inference_spinnaker2.py
+++ paper/03_rnn/Braille_inference_spinnaker2.py
@@ -3,6 +3,7 @@
 """Run Braille inference on SpiNNaker2."""
 
 import os
+from itertools import islice
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,38 +25,27 @@ device = torch.device("cpu")
 
 def input_array_to_spike_list(input_array):
     input_spikes = {}
+    spike_counts_s2 = 0
     for i, row in enumerate(input_array):
         input_spikes[i] = np.where(row == 1)[0].astype(int).tolist()
+        spike_counts_s2 += len(input_spikes[i])
     return input_spikes
 
 
 ### Load NIR Graph
 
 # load NIR model
-nir_model = nir.read("braille_flattened.nir")
+model_name = "retrained_zero"
+backend = "S2"  # or "brian2"
 
+if model_name.endswith("zero"):
+    reset_method = s2_nir.ResetMethod.ZERO
+elif model_name.endswith("subtract"):
+    reset_method = s2_nir.ResetMethod.SUBTRACT
+else:
+    raise Exception("unsupported reset")
 
-def apply_shape_to_one_to_one_nodes(node, new_shape):
-    print(new_shape)
-    print(np.array(new_shape).dtype)
-    node.input_type["input"] = np.array(new_shape)
-    node.output_type["output"] = np.array(new_shape)
-
-
-# apply shapes to CubaLIF nodes.
-apply_shape_to_one_to_one_nodes(
-    nir_model.nodes["lif1"], nir_model.nodes["fc1"].output_type["output"]
-)
-apply_shape_to_one_to_one_nodes(
-    nir_model.nodes["lif2"], nir_model.nodes["fc2"].output_type["output"]
-)
-
-# try if recurrent weights are in right order
-# nir_model.nodes["w_rec"].weight =  nir_model.nodes["w_rec"].weight.T
-
-# add recording of lif1
-# nir_model.nodes["output_l1"] = nir.Output(np.array([7]))
-# nir_model.edges.append(("lif1", "output_l1"))
+nir_model = nir.read(f"braille_{model_name}.nir")
 
 print("nodes:")
 for nodekey, node in nir_model.nodes.items():
@@ -64,20 +54,22 @@ print("edges:")
 for edge in nir_model.edges:
     print("\t", edge)
 
-net, inp, outp = s2_nir.from_nir(
-    nir_model,
-    outputs=["v", "spikes"],
-    discretization_timestep=1.0,
+# s2_nir.add_output_to_node("lif1.lif", nir_model, "ouput_lif1")
+
+cfg = s2_nir.ConversionConfig(
+    output_record=["v", "spikes"],
+    dt=0.0001,
     conn_delay=0,
-    scale_weights=False,
+    scale_weights=True,
+    reset=reset_method,
+    integrator=s2_nir.IntegratorMethod.FORWARD,
 )
+net, inp, outp = s2_nir.from_nir(nir_model, cfg)
+assert len(inp) == 1  # make sure there is only one input pop
 
 for pop in net.populations:
-    if pop.name == "lif1":
+    if pop.name == "lif1.lif":
         pop.set_max_atoms_per_core(10)
-        # pop.params["i_offset"] = 0.
-        print(pop.params)
-
 
 ### TEST DATA
 test_data_path = "data/ds_test.pt"
@@ -90,28 +82,29 @@ do_plot = False
 predicted_labels = []
 actual_labels = []
 
-for i in range(n_samples):
-    single_sample = next(iter(DataLoader(ds_test, batch_size=1, shuffle=True)))
+my_loader = DataLoader(ds_test, batch_size=1, shuffle=False)
+
+for single_sample in islice(my_loader, n_samples):
     sample = single_sample[0].numpy()  # shape: (1, 256, 12)
 
     spike_times = input_array_to_spike_list(sample[0, :, :].T)
 
     inp[0].params = spike_times
-
-    hw = hardware.SpiNNaker2Chip(eth_ip="192.168.1.25")  # use ethernet
-    hw = brian2_sim.Brian2Backend()
+    # inp[0].params = {}
 
     timesteps = sample.shape[1]
     net.reset()  # clear previous spikes and voltages
-    hw.run(net, timesteps, quantize_weights=False)
+
+    if backend == "S2":
+        hw = hardware.SpiNNaker2Chip(eth_ip="192.168.1.25")  # use ethernet
+        hw.run(net, timesteps, debug=False)
+    else:
+        hw = brian2_sim.Brian2Backend()
+        hw.run(net, timesteps, quantize_weights=False)
 
     spike_times = outp[0].get_spikes()
     voltages = outp[0].get_voltages()
     v_scale = outp[0].nir_v_scale
-    print(v_scale)
-
-    # print(spike_times)
-    # print(outp[0].params)
 
     n_output_spikes = np.zeros(len(spike_times))
     for i, s in spike_times.items():
@@ -139,3 +132,6 @@ predicted_labels = np.array(predicted_labels)
 actual_labels = np.array(actual_labels)
 n_correct = np.count_nonzero(predicted_labels == actual_labels)
 print("n_correct", n_correct, "out of", n_samples)
+accuracy = np.round(n_correct / n_samples * 100, 2)
+np.save(f"s2_accuracy_{model_name}.npy", accuracy)
+print(f"Test accuracy: {accuracy}%")
