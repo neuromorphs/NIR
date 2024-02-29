9aea1ab889c3bcfef8d4399ce779be0022603cd6
Alex Fan <alex.fan.q@gmail.com>
2024-02-29 23:35:50 +0800

    merge read and write into serialization

--- nir/__init__.py
+++ nir/__init__.py
@@ -6,8 +6,7 @@ Documentation: https://nnir.readthedocs.io
 from . import ir
 from .ir import *  # noqa: F403
 from .ir import typing  # noqa: F401
-from .read import read
-from .write import write
+from .serialization import read, write
 
 __all__ = ir.__all__ + ["read", "write"]
 
--- nir/serialization.py
+++ nir/serialization.py
@@ -2,6 +2,7 @@ import pathlib
 from typing import Any, Union
 
 import h5py
+import numpy as np
 
 import nir
 
@@ -128,3 +129,23 @@ def read_version(filename: Union[str, pathlib.Path]) -> str:
     does not exist in the file."""
     with h5py.File(filename, "r") as f:
         return f["version"][()].decode("utf8")
+
+
+def write(filename: Union[str, pathlib.Path], graph: nir.typing.NIRNode) -> None:
+    """Write a NIR to a HDF5 file."""
+
+    def write_recursive(group: h5py.Group, node: dict) -> None:
+        for k, v in node.items():
+            if isinstance(v, str):
+                group.create_dataset(k, data=v, dtype=h5py.string_dtype())
+            elif isinstance(v, np.ndarray):
+                group.create_dataset(k, data=v, dtype=v.dtype)
+            elif isinstance(v, dict):
+                write_recursive(group.create_group(str(k)), v)
+            else:
+                group.create_dataset(k, data=v)
+
+    with h5py.File(filename, "w") as f:
+        f.create_dataset("version", data=nir.version)
+        node_group = f.create_group("node")
+        write_recursive(node_group, graph.to_dict())
