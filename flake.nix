b1bbe6d40eed689e133de7e3b3e5aa2b67621f28
jegp <jens@jepedersen.dk>
2025-10-01 12:10:10 +0200

    Added some units to the primitives

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
-  description = "Neuromorphic Intermediate Representation reference implementation"; 
+  description = "Neuromorphic Intermediate Representation reference implementation";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; 
-      in { 
-        devShells.default = 
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default =
           let pythonPackages = pkgs.python3Packages;
           in pkgs.mkShell rec {
             name = "impurePythonEnv";
@@ -20,11 +20,15 @@
               pythonPackages.h5py
               pythonPackages.black
               pythonPackages.pytest
+              pkgs.nodejs_24
+              pkgs.uv
               pkgs.ruff
               pkgs.autoPatchelfHook
             ];
             postVenvCreation = ''
               unset SOURCE_DATE_EPOCH
+              uv pip install --group dev
+              uv pip install -r docs/requirements.txt
             '';
             postShellHook = ''
               unset SOURCE_DATE_EPOCH
